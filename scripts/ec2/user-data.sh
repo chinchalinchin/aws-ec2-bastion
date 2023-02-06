c13a7f9802ca1e874fe63552a49eb7704634760c
Grant Moore <625518@bah.com>
2023-02-06 08:34:41 -0500

    updating userdata

--- scripts/ec2/user-data.sh
+++ scripts/ec2/user-data.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
 
 ## SYSTEM DEPENDENCIES
-apt update -y
-apt install -y \
-    ubuntu-desktop \
-    xrdp \
-    xfce4 \
-    xfce4-session \
-    docker.io \
-    docker-compose
-apt-get update -y
-apt-get install -y \
-    apt-transport-https
-    ca-certificates \
-    curl \
-    unzip \
-    gnupg \
-    software-properties-common \
-    jq
-    gnupg \
-    lsb-release
+# apt update -y
+# apt install -y \
+#     docker.io \
+#     docker-compose
 
 ## REPOSITORY INSTALLATIONS
 ### DOCKER 
+echo "Manually adding Docker apt repository..."
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
     gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -30,6 +16,7 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
     tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 ### TERRAFORM
+echo "Manually adding Terraform apt repository..."
 wget -O- https://apt.releases.hashicorp.com/gpg | \
     gpg --dearmor | \
     tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
@@ -37,7 +24,10 @@ echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://
     tee /etc/apt/sources.list.d/hashicorp.list
 
 
+echo "Updating apt-get repositories..."
 apt-get update -y
+
+echo "Installing apt-get dependencies..."
 apt-get install -y \
     terraform \
     docker-ce \
@@ -47,19 +37,13 @@ apt-get install -y \
 
 ## SOURCE INSTALLATIONS
 ### AWS CLI
+echo "Manually installing AWS CLI..."
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
 
-### xRDP CONFIGURATION
-# open RDP port
-ufw allow 3389/tcp
-# set default GUI
-echo xfce4-session >~/.xsession
-sudo sed -i.bak '/fi/a #xrdp multiple users configuration \n xfce-session \n' /etc/xrdp/startwm.sh
-service xrdp restart
-
 ### ECR LOGIN
+echo "Logging into AWS ECR..."
 aws ecr get-login-password \
     --region $AWS_DEFAULT_REGION |\
     docker login \
